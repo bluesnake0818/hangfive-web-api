b44c411af0c72a16e2f1337c8e0bfe82d1a8976f
Jubin Choi <ddonut@hangfive.co>
2022-05-14 17:05:52 +0900

    api gateway 용 환경변수 추가

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 import os
 
 DATABASE_URL = os.getenv('DATABASE_URL')
-
+API_URL = "https://kqomb31si4.execute-api.ap-northeast-2.amazonaws.com/"
+API_KEY = os.getenv('API_KEY')
 class Config:
   SQLALCHEMY_ECHO = False
   SQLALCHEMY_TRACK_MODIFICATIONS = False
